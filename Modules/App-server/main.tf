26b922a837ed3ac6390560896224e39ae7de2690
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-12-05 15:30:57 +0000

    Heating service feature created

--- Modules/App-server/main.tf
+++ Modules/App-server/main.tf
@@ -11,19 +11,7 @@ resource "aws_instance" "lighting_app_server" {
     Name = "Lighting App Server${count.index+1}"
   }
 }
-# resource "aws_instance" "heating_app_server" {
-#   instance_type          = var.instance_type
-#   key_name               = var.key_name
-#   ami                    = "ami-0505148b3591e4c07"
-#   count                  = var.server_count
-#   vpc_security_group_ids = [var.security_group_id]
-#   subnet_id              = var.public_subnets[1]
-#   availability_zone = var.availability_zone[1]
-#   associate_public_ip_address = true
-#   tags = {
-#     Name = "Lighting App Server${count.index+1}"
-#   }
-# }
+
 # resource "aws_instance" "status_app_server" {
 #   instance_type          = var.instance_type
 #   key_name               = var.key_name
--- Modules/DynamoDB/main.tf
+++ Modules/DynamoDB/main.tf
@@ -30,3 +30,19 @@ resource "aws_dynamodb_table" "lighting_table" {
         Environment = "production"
     }
 }
+resource "aws_dynamodb_table" "heating_table" {
+    name = "heating"
+    billing_mode = "PROVISIONED"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = "id"
+    attribute {
+      name = "id"
+      type = "N"
+    }
+
+    tags = {
+        Name = "Heating-Table"
+        Environment = "production"
+    }
+}
