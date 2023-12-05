379424ee280024a224f7c316f8796663ef10936f
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-12-05 14:34:43 +0000

    light can be posted into DynamoDB

--- Modules/DynamoDB/main.tf
+++ Modules/DynamoDB/main.tf
@@ -1,30 +1,30 @@
 resource "aws_dynamodb_table" "lighting_table" {
-    name = "LightingAppTable"
+    name = "Lighting-Table"
     billing_mode = "PROVISIONED"
     read_capacity = 20
     write_capacity = 20
-    hash_key = "Id"
+    hash_key = "id"
     attribute {
-      name = "Id"
+      name = "id"
       type = "N"
     }
-    attribute {
-      name = "Location"
-      type = "S"
-    }
-    attribute {
-      name = "Status"
-      type = "B"
-    }
-    global_secondary_index {
-      name = "locIndex"
-      hash_key = "Location"
-      range_key = "Status"
-      write_capacity = 10
-      read_capacity = 10
-      projection_type = "INCLUDE"
-      non_key_attributes = ["Id"]
-    }
+#     attribute {
+#       name = "location"
+#       type = "S"
+#     }
+#  attribute {
+#   name= "status"
+#   type = "B"
+#  }
+#     global_secondary_index {
+#       name = "index"
+#       hash_key = "location"
+#       range_key = "status"
+#       write_capacity = 10
+#       read_capacity = 10
+#       projection_type = "INCLUDE"
+#       non_key_attributes = ["id"]
+#     }
     tags = {
         Name = "Lighting-table"
         Environment = "production"
