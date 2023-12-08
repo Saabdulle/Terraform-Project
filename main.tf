d6b80311615ec4e1515a70a6b011d266ade34653
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-12-08 09:33:18 +0000

    Auto Scaling feature created

--- main.tf
+++ main.tf
@@ -19,17 +19,29 @@ module "Apps-Server" {
   public_subnets    = module.Networking.public_subnets_ids
 }
 module "Load-Balancer" {
-  source = "./Modules/Load-Balancer"
-  vpc_id = module.Networking.vpc_id
+  source             = "./Modules/Load-Balancer"
+  vpc_id             = module.Networking.vpc_id
   security_group_ids = module.Security.security_group_id
-  public_subnets = module.Networking.public_subnets_ids
-  instance_ids = module.Apps-Server.instance_ids
+  public_subnets     = module.Networking.public_subnets_ids
+  instance_ids       = module.Apps-Server.instance_ids
+  # ami_images = var.ami_images
 }
 module "Auto-scaling" {
-  source = "./Modules/Auto-Scaling"
+  source             = "./Modules/Auto-Scaling"
+  instance_type      = var.instance_type
+  public_subnets     = module.Networking.public_subnets_ids
+  key_name           = var.key_name
+  server_names       = var.server_names
+  instance_ids       = module.Apps-Server.instance_ids
+  security_group_ids = module.Security.security_group_id
+  min_size           = var.min_size
+  max_size           = var.max_size
+  desired_size       = var.desired_size
+  target_group_ids   = module.Load-Balancer.target_group_ids
+  ami_images         = var.ami_images
 }
 module "DynamoDB" {
-  source = "./Modules/DynamoDB"
-  table_tags = var.table_tags
+  source      = "./Modules/DynamoDB"
+  table_tags  = var.table_tags
   table_names = var.table_names
 }
